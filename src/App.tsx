3d49fc552495f88e09e8857e4f314e82c1909c50
ebabarykin.o <ebabarykin.o@adq.ae>
2024-09-26 08:57:44 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import {useState} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -12,10 +12,10 @@ function App() {
         <>
             <div>
                 <a href="https://vitejs.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
+                    <img src={viteLogo} className="logo" alt="Vite logo"/>
                 </a>
                 <a href="https://react.dev" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
+                    <img src={reactLogo} className="logo react" alt="React logo"/>
                 </a>
             </div>
             <h1>Vite + React</h1>
@@ -31,7 +31,11 @@ function App() {
                 </button>
             </div>
             <div className="card">
-                <button onClick={() => WebApp.showScanQrPopup({text:`some text`}, (text)=> WebApp.showAlert(text))}>
+                <button
+                    onClick={() => WebApp.showScanQrPopup({text: `some text`}, (text) => {
+                        WebApp.closeScanQrPopup()
+                        WebApp.showAlert(text)
+                    })}>
                     scan qr
                 </button>
             </div>
